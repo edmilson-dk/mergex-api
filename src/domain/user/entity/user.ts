707fb9fe9c0a9075e52e168705572add2996c5fd
edmilson-dk <droidvidaboa@gmail.com>
2021-11-10 11:42:04 -0300

    feat: added create email  private method

--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -1,13 +1,18 @@
-import { Either, left } from '@shared/error-handler/either';
-import { isValidName } from '@shared/validators';
+import { Either, left, right } from '@shared/error-handler/either';
+import { isValidEmail, isValidName } from '@shared/validators';
 import { UserCreateDto } from '../dtos';
+import { InvalidEmailError } from '../use-cases/errors/invalidEmailError';
 import { InvalidNameError } from '../use-cases/errors/invalidNameError';
-import { right } from '../../../shared/error-handler/either/index';
 
 export class User {
   private createName(name: string): Either<InvalidNameError, string> {
     const isValid = isValidName(name);
     return isValid ? right(name) : left(new InvalidNameError(name));
+  }
+
+  private createEmail(email: string): Either<InvalidEmailError, string> {
+    const isValid = isValidEmail(email);
+    return isValid ? right(email) : left(new InvalidEmailError(email));
   }
 
   public build(data: UserCreateDto) {}
