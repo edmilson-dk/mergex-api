e4b64de6373a2b2a6a49b06d479e9c41c8cb3685
edmilson-dk <droidvidaboa@gmail.com>
2021-11-10 11:07:31 -0300

    feat: created create user response types

--- src/domain/user/use-cases/ports/responses.ts
+++ src/domain/user/use-cases/ports/responses.ts
@@ -1,3 +1,9 @@
-export type CreateUserResponse = {
-  id: string;
-};
+import { Either } from '@shared/error-handler/either';
+import { InvalidEmailError } from '../errors/invalidEmailError';
+import { InvalidNameError } from '../errors/invalidNameError';
+import { InvalidPasswordError } from '../errors/invalidPassword';
+import { InvalidUsernameError } from '../errors/invalidUsername';
+
+export type CreateUserSuccess = { id: string };
+export type CreateUserFailure = InvalidEmailError | InvalidNameError | InvalidPasswordError | InvalidUsernameError;
+export type CreateUserResponse = Either<CreateUserFailure, CreateUserSuccess>;
