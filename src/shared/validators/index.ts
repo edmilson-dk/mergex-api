98106606e6a3bd3cb76cc60b78b9ed1087289280
edmilson-dk <droidvidaboa@gmail.com>
2021-11-10 11:17:01 -0300

    feat: created name validate

--- src/shared/validators/index.ts
+++ src/shared/validators/index.ts
@@ -1,13 +1,26 @@
 import Joi from 'joi';
 
-const regex =
+const emailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const schema = Joi.object({
-  email: Joi.string().email().required().regex(new RegExp(regex)),
+
+const nameRegex = /^[a-zA-Z]+$/;
+
+const emailSchema = Joi.object({
+  email: Joi.string().email().required().regex(new RegExp(emailRegex)),
+});
+
+const nameSchema = Joi.object({
+  name: Joi.string().required().regex(new RegExp(nameRegex)).min(3).max(30),
 });
 
 export function isValidEmail(email: string): boolean {
   if (!email) return false;
-  const result = schema.validate({ email });
+  const result = emailSchema.validate({ email });
+  return result.error ? false : true;
+}
+
+export function isValidName(name: string): boolean {
+  if (!name) return false;
+  const result = nameSchema.validate({ name });
   return result.error ? false : true;
 }
