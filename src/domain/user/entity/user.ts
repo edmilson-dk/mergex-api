1e8c571f07c4eb1a2ee4e506d568f470fdef1b21
edmilson-dk <droidvidaboa@gmail.com>
2021-11-10 11:43:10 -0300

    feat: added create password private method

--- src/domain/user/entity/user.ts
+++ src/domain/user/entity/user.ts
@@ -1,8 +1,9 @@
 import { Either, left, right } from '@shared/error-handler/either';
-import { isValidEmail, isValidName } from '@shared/validators';
+import { isValidEmail, isValidName, isValidPassword } from '@shared/validators';
 import { UserCreateDto } from '../dtos';
 import { InvalidEmailError } from '../use-cases/errors/invalidEmailError';
 import { InvalidNameError } from '../use-cases/errors/invalidNameError';
+import { InvalidPasswordError } from '../use-cases/errors/invalidPassword';
 
 export class User {
   private createName(name: string): Either<InvalidNameError, string> {
@@ -13,6 +14,11 @@ export class User {
   private createEmail(email: string): Either<InvalidEmailError, string> {
     const isValid = isValidEmail(email);
     return isValid ? right(email) : left(new InvalidEmailError(email));
+  }
+
+  private createPassword(password: string): Either<InvalidPasswordError, string> {
+    const isValid = isValidPassword(password);
+    return isValid ? right(password) : left(new InvalidPasswordError(password));
   }
 
   public build(data: UserCreateDto) {}
