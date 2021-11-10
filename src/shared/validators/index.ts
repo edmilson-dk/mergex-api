368499d5216fb59dbee82fa22ac935644bfb34df
edmilson-dk <droidvidaboa@gmail.com>
2021-11-10 11:23:56 -0300

    feat: created username validate

--- src/shared/validators/index.ts
+++ src/shared/validators/index.ts
@@ -2,15 +2,19 @@ import Joi from 'joi';
 
 const emailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-const nameRegex = /^[a-zA-Z]+$/;
+// name may contain spaces
+const nameRegex = /^[a-zA-Z0-9 ]+$/;
+// username start with @ and not contain space
+const userNameRegex = /^@[a-zA-Z0-9_]{3,30}$/;
 
 const emailSchema = Joi.object({
   email: Joi.string().email().required().regex(new RegExp(emailRegex)),
 });
-
 const nameSchema = Joi.object({
   name: Joi.string().required().regex(new RegExp(nameRegex)).min(3).max(30),
+});
+const usernameSchema = Joi.object({
+  username: Joi.string().required().regex(new RegExp(userNameRegex)).min(3).max(30),
 });
 
 export function isValidEmail(email: string): boolean {
@@ -22,5 +26,11 @@ export function isValidEmail(email: string): boolean {
 export function isValidName(name: string): boolean {
   if (!name) return false;
   const result = nameSchema.validate({ name });
+  return result.error ? false : true;
+}
+
+export function isValidUsername(username: string): boolean {
+  if (!username) return false;
+  const result = usernameSchema.validate({ username });
   return result.error ? false : true;
 }
