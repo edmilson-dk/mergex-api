e85d7e010c8604a53df205f583a3ed735407c43a
edmilson-dk <droidvidaboa@gmail.com>
2021-11-13 12:20:18 -0300

    feat: created user stored dto

--- src/domain/user/dtos/index.ts
+++ src/domain/user/dtos/index.ts
@@ -9,3 +9,21 @@ export type UserCreateDto = {
   avatarUrl: string;
   bio: string;
 };
+
+export type UserStoredDto = {
+  id: string;
+  email: string;
+  name: string;
+  username: string;
+  githubId: string;
+  githubUsername: string;
+  githubProfile: string;
+  avatarUrl: string;
+  bannerUrl: string;
+  website: string;
+  location: string;
+  dateOfBirth: string;
+  isDisabled: boolean;
+  bio: string;
+  createdAt: string;
+};
