988c97b54f1929b5991fdfc345bb1e1240fae31a
edmilson-dk <droidvidaboa@gmail.com>
2021-11-13 12:19:27 -0300

    feat: added news methods types

--- src/application/repositories/user/index.ts
+++ src/application/repositories/user/index.ts
@@ -1,13 +1,11 @@
 import { UserCreateDto } from '@domain/user/dtos';
-
-export type FindUserRepositoryResponse = {
-  id: string | null;
-  githubId: string | null;
-};
+import { FindUserRepositoryResponse, GetUserStoredRepositoryResponse } from './ports';
 
 export interface IUserRepository {
   findByGithubId(githubId: string): Promise<FindUserRepositoryResponse>;
   findByEmail(email: string): Promise<FindUserRepositoryResponse>;
   findByUsername(username: string): Promise<FindUserRepositoryResponse>;
+  getUserByEmail(email: string): Promise<GetUserStoredRepositoryResponse>;
+  getUserByGithubId(githubId: string): Promise<GetUserStoredRepositoryResponse>;
   createUser(data: UserCreateDto): Promise<{ id: string }>;
 }
