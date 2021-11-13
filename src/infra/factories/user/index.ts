a3e15f58640f80da0884dd6d3d9dd434d1d99418
edmilson-dk <droidvidaboa@gmail.com>
2021-11-13 13:37:51 -0300

    feat: added makers of news controllers

--- src/infra/factories/user/index.ts
+++ src/infra/factories/user/index.ts
@@ -4,6 +4,8 @@ import { UserUseCases } from '@application/use-cases/user';
 import { CreateUserController } from '@infra/presentation/controllers/user/createUserController';
 import { GithubServicesByApi } from '@infra/services/github';
 import { PrismaPgUserRepository } from '@infra/repositories/user';
+import { AuthUserByGithubController } from '@infra/presentation/controllers/user/authUserByGithubController';
+import { AuthUserByEmailController } from '@infra/presentation/controllers/user/authUserByEmailController';
 
 const userRepository = new PrismaPgUserRepository();
 const userUseCases = new UserUseCases(userRepository);
@@ -11,5 +13,15 @@ const githubServices = new GithubServicesByApi();
 
 export async function makeCreateUserController(req: Request, res: Response) {
   const controller = new CreateUserController(userUseCases, githubServices);
+  return await controller.handle(req, res);
+}
+
+export async function makeAuthUserByGithubController(req: Request, res: Response) {
+  const controller = new AuthUserByGithubController(userUseCases, githubServices);
+  return await controller.handle(req, res);
+}
+
+export async function makeAuthUserByEmailController(req: Request, res: Response) {
+  const controller = new AuthUserByEmailController(userUseCases);
   return await controller.handle(req, res);
 }
