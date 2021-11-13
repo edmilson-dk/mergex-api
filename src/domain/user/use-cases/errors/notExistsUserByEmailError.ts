a82db44ce439a85149132b9a32265916a5047c4d
edmilson-dk <droidvidaboa@gmail.com>
2021-11-13 13:07:09 -0300

    feat: created not exists user by email error

---
+++ src/domain/user/use-cases/errors/notExistsUserByEmailError.ts
@@ -0,0 +1,6 @@
+export class NotExistsUserByEmailError extends Error implements DomainError {
+  constructor(email: string) {
+    super(`User with email ${email} not exists`);
+    this.name = 'NotExistsUserByEmailError';
+  }
+}
