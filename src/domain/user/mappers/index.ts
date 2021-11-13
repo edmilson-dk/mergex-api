7d05db02c3795ea60a1cb7871e461b224fd7d6f2
edmilson-dk <droidvidaboa@gmail.com>
2021-11-13 13:09:13 -0300

    feat: created news mapper method

--- src/domain/user/mappers/index.ts
+++ src/domain/user/mappers/index.ts
@@ -1,4 +1,4 @@
-import { UserCreateDto } from '../dtos';
+import { UserCreateDto, UserDbStoredDto, UserDto, UserStoredDto } from '../dtos';
 
 export class UserMappers {
   static toCreateUserDto(params: any): UserCreateDto {
@@ -12,6 +12,47 @@ export class UserMappers {
       githubProfile: params.githubProfile,
       githubUsername: params.githubUsername,
       username: params.username.toLowerCase(),
+    };
+  }
+
+  static fromDbToUserStoredDto(params: UserDbStoredDto): UserStoredDto {
+    return {
+      id: params.id,
+      email: params.email,
+      username: params.username,
+      name: params.name,
+      bio: params.bio,
+      githubProfile: params.github_profile,
+      githubUsername: params.github_username,
+      githubId: params.github_id,
+      password: params.password,
+      createdAt: params.created_at.toISOString(),
+      website: params.website,
+      dateOfBirth: params.date_of_birth ? params.date_of_birth.toISOString() : null,
+      location: params.location,
+      bannerUrl: params.banner_url,
+      avatarUrl: params.avatar_url,
+      isDisabled: params.is_disabled,
+    };
+  }
+
+  static toUserDto(params: UserStoredDto): UserDto {
+    return {
+      id: params.id,
+      email: params.email,
+      username: params.username,
+      name: params.name,
+      bio: params.bio,
+      githubProfile: params.githubProfile,
+      githubUsername: params.githubUsername,
+      githubId: params.githubId,
+      createdAt: params.createdAt,
+      website: params.website,
+      dateOfBirth: params.dateOfBirth,
+      location: params.location,
+      bannerUrl: params.bannerUrl,
+      avatarUrl: params.avatarUrl,
+      isDisabled: params.isDisabled,
     };
   }
 }
