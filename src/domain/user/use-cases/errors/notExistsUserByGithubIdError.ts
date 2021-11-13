35acaf6071a45e032b50d43436f3a912cef73af5
edmilson-dk <droidvidaboa@gmail.com>
2021-11-13 13:07:24 -0300

    feat: created not exists user by github id error

---
+++ src/domain/user/use-cases/errors/notExistsUserByGithubIdError.ts
@@ -0,0 +1,6 @@
+export class NotExistsUserByGithubIdError extends Error implements DomainError {
+  constructor(githubId: string) {
+    super(`User with github id ${githubId} not exists`);
+    this.name = 'NotExistsUserByGithubIdError';
+  }
+}
