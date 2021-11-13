819eb008b80bbb50f072ad138a3f3306255651b2
edmilson-dk <droidvidaboa@gmail.com>
2021-11-13 12:19:45 -0300

    feat: added news methods types

--- src/domain/user/use-cases/index.ts
+++ src/domain/user/use-cases/index.ts
@@ -1,7 +1,9 @@
-import { UserCreateDto } from '../dtos';
-import { CreateUserUseCaseResponse } from './ports/responses';
+import { UserCreateDto, UserStoredDto } from '../dtos';
+import { CreateUserUseCaseResponse, GetUserByEmailRequest, GetUserByGithubRequest } from './ports';
 
 export interface IUserUseCases {
   createUser: (data: UserCreateDto) => Promise<CreateUserUseCaseResponse>;
   existsUsername: (username: string) => Promise<boolean>;
+  getUserByEmail: (data: GetUserByEmailRequest) => Promise<UserStoredDto>;
+  getUserByGithub: (data: GetUserByGithubRequest) => Promise<UserStoredDto>;
 }
--- src/domain/user/use-cases/ports/responses.ts
+++
@@ -1,7 +0,0 @@
-import { Either } from '@shared/error-handler/either';
-import { ExistingByEmailUserError } from '../errors/existingByEmailUserError';
-import { ExistingByGithubIdUserError } from '../errors/existingByGithubIdUserError';
-
-export type CreateUserUseCaseSuccess = { id: string };
-export type CreateUserUseCaseFailure = ExistingByGithubIdUserError | ExistingByEmailUserError;
-export type CreateUserUseCaseResponse = Either<CreateUserUseCaseFailure, CreateUserUseCaseSuccess>;
