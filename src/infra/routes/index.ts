31e33f59d08e7bda8fe7fcc68f1bbe4458e1893b
edmilson-dk <droidvidaboa@gmail.com>
2021-11-14 13:26:28 -0300

    feat: added post routes

--- src/infra/routes/index.ts
+++ src/infra/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import { postRoutes } from './post';
 import { userRoutes } from './user';
 
 export const routes = Router();
 
 routes.use('/user', userRoutes);
+routes.use('/post', postRoutes);
