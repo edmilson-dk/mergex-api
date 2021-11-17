69c76883c47d4b2f524e3e13188e240d680d6ecc
edmilson-dk <droidvidaboa@gmail.com>
2021-11-17 10:47:27 -0300

    feat: added update user profile router PUT

--- src/infra/routes/user/index.ts
+++ src/infra/routes/user/index.ts
@@ -4,10 +4,13 @@ import {
   makeAuthUserByEmailController,
   makeAuthUserByGithubController,
   makeCreateUserController,
+  makeUpdateUserProfileController,
 } from '@infra/factories/user';
+import { authUserJwtMiddleware } from '../middlewares/auth-middlewares';
 
 export const userRoutes = Router();
 
 userRoutes.post('/register', makeCreateUserController);
 userRoutes.post('/login/github', makeAuthUserByGithubController);
 userRoutes.post('/login/email', makeAuthUserByEmailController);
+userRoutes.put('/profile', authUserJwtMiddleware, makeUpdateUserProfileController);
