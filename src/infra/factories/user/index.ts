add4f5a65a3237385b17e6e721e694edfc6e1f76
edmilson-dk <droidvidaboa@gmail.com>
2021-11-17 10:49:05 -0300

    feat: created make update user controller

--- src/infra/factories/user/index.ts
+++ src/infra/factories/user/index.ts
@@ -6,6 +6,7 @@ import { GithubServicesByApi } from '@infra/services/github';
 import { PrismaPgUserRepository } from '@infra/repositories/user';
 import { AuthUserByGithubController } from '@infra/presentation/controllers/user/authUserByGithubController';
 import { AuthUserByEmailController } from '@infra/presentation/controllers/user/authUserByEmailController';
+import { UpdateUserProfileController } from '@infra/presentation/controllers/user/updateUserProfileController';
 
 const userRepository = new PrismaPgUserRepository();
 const userUseCases = new UserUseCases(userRepository);
@@ -23,5 +24,10 @@ export async function makeAuthUserByGithubController(req: Request, res: Response
 
 export async function makeAuthUserByEmailController(req: Request, res: Response) {
   const controller = new AuthUserByEmailController(userUseCases);
+  return await controller.handle(req, res);
+}
+
+export async function makeUpdateUserProfileController(req: Request, res: Response) {
+  const controller = new UpdateUserProfileController(userUseCases);
   return await controller.handle(req, res);
 }
