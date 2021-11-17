4a95d2f07aea3167a9698fa20b1d269fd22efdaa
edmilson-dk <droidvidaboa@gmail.com>
2021-11-17 10:50:49 -0300

    feat: added user use cases update profile method type

--- src/domain/user/use-cases/index.ts
+++ src/domain/user/use-cases/index.ts
@@ -1,14 +1,16 @@
-import { UserCreateDto } from '../dtos';
+import { UserCreateDto, UserProfileDto } from '../dtos';
 import {
   CreateUserUseCaseResponse,
   AuthUserByEmailRequest,
   AuthUserByGithubRequest,
   AuthUserByEmailUseCaseResponse,
   AuthUserByGithubUseCaseResponse,
+  UpdateUserProfileUseCaseResponse,
 } from './ports';
 
 export interface IUserUseCases {
   createUser: (data: UserCreateDto) => Promise<CreateUserUseCaseResponse>;
   authUserByEmail: (data: AuthUserByEmailRequest) => Promise<AuthUserByEmailUseCaseResponse>;
   authUserByGithubId: (data: AuthUserByGithubRequest) => Promise<AuthUserByGithubUseCaseResponse>;
+  updateUserProfile: (data: UserProfileDto, userId: string) => Promise<UpdateUserProfileUseCaseResponse>;
 }
