d7f3067880453c86ac2094bbb46283a43739343f
edmilson-dk <droidvidaboa@gmail.com>
2021-11-17 10:50:27 -0300

    feat: created update user profile use case response type

--- src/domain/user/use-cases/ports/index.ts
+++ src/domain/user/use-cases/ports/index.ts
@@ -1,14 +1,22 @@
-import { UserDbStoredDto, UserDto } from '@domain/user/dtos';
+import { UserDbStoredDto, UserDto, UserProfileDto } from '@domain/user/dtos';
+import { InvalidBioError } from '@domain/user/entity/errors/invalidBioError';
+import { InvalidNameError } from '@domain/user/entity/errors/invalidNameError';
+import { InvalidUsernameError } from '@domain/user/entity/errors/invalidUsername';
 import { CreateUserEntityFailure } from '@domain/user/entity/ports';
 import { Either } from '@shared/error-handler/either';
 import { ExistingByEmailUserError } from '../errors/existingByEmailUserError';
 import { ExistingByGithubIdUserError } from '../errors/existingByGithubIdUserError';
+import { ExistingByUsernameUserError } from '../errors/existingByUsernameError';
 import { InvalidUserCredentialsError } from '../errors/invalidUserCredentialsError';
 import { NotExistsUserByEmailError } from '../errors/notExistsUserByEmailError';
 import { NotExistsUserByGithubIdError } from '../errors/notExistsUserByGithubIdError';
 
 export type CreateUserUseCaseSuccess = { id: string };
-export type CreateUserUseCaseFailure = ExistingByGithubIdUserError | ExistingByEmailUserError | CreateUserEntityFailure;
+export type CreateUserUseCaseFailure =
+  | ExistingByGithubIdUserError
+  | ExistingByEmailUserError
+  | ExistingByUsernameUserError
+  | CreateUserEntityFailure;
 export type CreateUserUseCaseResponse = Either<CreateUserUseCaseFailure, CreateUserUseCaseSuccess>;
 
 export type AuthUserByEmailRequest = {
@@ -28,3 +36,11 @@ export type AuthUserByEmailUseCaseResponse = Either<AuthUserByEmailUseCaseFailur
 export type AuthUserByGithubUseCaseSuccess = UserDto;
 export type AuthUserByGithubUseCaseFailure = NotExistsUserByGithubIdError | InvalidUserCredentialsError;
 export type AuthUserByGithubUseCaseResponse = Either<AuthUserByGithubUseCaseFailure, AuthUserByGithubUseCaseSuccess>;
+
+export type UpdateUserProfileUseCaseSuccess = UserProfileDto;
+export type UpdateUserProfileUseCaseFailure =
+  | ExistingByUsernameUserError
+  | InvalidBioError
+  | InvalidUsernameError
+  | InvalidNameError;
+export type UpdateUserProfileUseCaseResponse = Either<UpdateUserProfileUseCaseFailure, UpdateUserProfileUseCaseSuccess>;
