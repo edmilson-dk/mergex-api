c4f0300ec5575f3d36e6669fb5b6bac3f6661245
edmilson-dk <droidvidaboa@gmail.com>
2021-11-20 15:22:43 -0300

    feat: added update user profile

--- src/infra/routes/user/index.ts
+++ src/infra/routes/user/index.ts
@@ -1,16 +1,26 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import {
   makeAuthUserByEmailController,
   makeAuthUserByGithubController,
   makeCreateUserController,
+  makeUpdateUserAvatarController,
   makeUpdateUserProfileController,
 } from '@infra/factories/user';
 import { authUserJwtMiddleware } from '../middlewares/auth-middlewares';
+import { multerImageUploadConfig } from '@shared/multer';
 
 export const userRoutes = Router();
+const multerImageUpload = multer(multerImageUploadConfig);
 
 userRoutes.post('/register', makeCreateUserController);
 userRoutes.post('/login/github', makeAuthUserByGithubController);
 userRoutes.post('/login/email', makeAuthUserByEmailController);
 userRoutes.put('/profile', authUserJwtMiddleware, makeUpdateUserProfileController);
+userRoutes.put(
+  '/profile/avatar',
+  multerImageUpload.single('image'),
+  authUserJwtMiddleware,
+  makeUpdateUserAvatarController,
+);
