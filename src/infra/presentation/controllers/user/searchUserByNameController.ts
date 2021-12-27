698c203114cb8af6a329e7258590792a2ebd1896
edmilson-dk <droidvidaboa@gmail.com>
2021-12-27 07:57:50 -0300

    feat: created search user by name controller

---
+++ src/infra/presentation/controllers/user/searchUserByNameController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+
+import { IUserUseCases } from '@domain/user/use-cases';
+
+export class SearchUserByNameController {
+  private readonly userUseCase: IUserUseCases;
+
+  constructor(userUseCases: IUserUseCases) {
+    this.userUseCase = userUseCases;
+  }
+
+  public async handle(req: Request, res: Response) {
+    try {
+      const name = req.query.name as string;
+
+      if (!name) {
+        return res.status(400).send({ message: 'query [name] is required' });
+      }
+
+      const users = await this.userUseCase.searchUserByName(name);
+
+      return res.status(200).send({ users });
+    } catch (err) {
+      return res.status(500).send({ message: err });
+    }
+  }
+}
