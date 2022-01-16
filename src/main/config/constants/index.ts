f2606f893698bcdb85cd50c0b20ae44371305aa2
edmilson-dk <droidvidaboa@gmail.com>
2022-01-16 09:30:26 -0300

    feat: added news env variables

--- src/main/config/constants/index.ts
+++ src/main/config/constants/index.ts
@@ -2,12 +2,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const ENVS = {
-  NODE_ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
-  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
-  GITHUB_AUTH_URL: process.env.GITHUB_AUTH_URL,
-  GITHUB_ACCESS_TOKEN_URL: process.env.GITHUB_ACCESS_TOKEN_URL,
-  GITHUB_API_USER_URL: process.env.GITHUB_API_USER_URL,
-  SECRET_KEY: process.env.SECRET_KEY,
+  NODE_ENV: process.env.NODE_ENV as string,
+  PORT: process.env.PORT as string,
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID as string,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET as string,
+  GITHUB_AUTH_URL: process.env.GITHUB_AUTH_URL as string,
+  GITHUB_ACCESS_TOKEN_URL: process.env.GITHUB_ACCESS_TOKEN_URL as string,
+  GITHUB_API_USER_URL: process.env.GITHUB_API_USER_URL as string,
+  SECRET_KEY: process.env.SECRET_KEY as string,
+  GITHUB_AVATAR_SAVE_PATH: process.env.GITHUB_AVATAR_SAVE_PATH as string,
+  IMAGE_UPLOAD_PATH: process.env.IMAGE_UPLOAD_PATH as string,
 };
